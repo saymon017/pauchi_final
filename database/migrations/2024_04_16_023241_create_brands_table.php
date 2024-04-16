f4a6343d3abdeb669d30a1f07a5a5318fcbc5f45
Saymon017 <simon.ramirez7000@gmail.com>
2024-04-16 13:20:33 -0500

    modelos

--- database/migrations/2024_04_16_023241_create_brands_table.php
+++ database/migrations/2024_04_16_023241_create_brands_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('slug')->unique();
-            $table->string('images')->nullable();
+            $table->string('image')->nullable();
             $table->string('is_active')->default(true);
             $table->timestamps();
         });
