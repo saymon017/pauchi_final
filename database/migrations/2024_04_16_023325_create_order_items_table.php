017fde35290ba90c898b690a0b1b84d171efc736
Saymon017 <simon.ramirez7000@gmail.com>
2024-04-16 11:06:34 -0500

    migraciones2

--- database/migrations/2024_04_16_023325_create_order_items_table.php
+++ database/migrations/2024_04_16_023325_create_order_items_table.php
@@ -15,6 +15,9 @@ return new class extends Migration
             $table->id();
             $table->foreignId('order_id')->constrained('orders')->cascadeOnDelete();
             $table->foreignId('product_id')->constrained('products')->cascadeOnDelete();
+            $table->integer('quantity')->default(1);
+            $table->decimal('unit_amount', 10, 2)->nullable();
+            $table->decimal('total_amount', 10, 2)->nullable();
             $table->timestamps();
         });
     }
